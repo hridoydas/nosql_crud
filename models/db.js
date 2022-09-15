4f0945442845dce7ac400e37c1eea8c5df291302
hridoydas <hdas@cisscom.com>
2022-09-16 00:30:14 +0600

    live mongo atlas connected

--- models/db.js
+++ models/db.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
-    if (!err) { console.log('MongoDB Connection Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
-});
-
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+}
+mongoose
+  .connect("mongodb+srv://hridoy:test1234@cluster0.9db0k05.mongodb.net/EmployeeDB?retryWrites=true&w=majority", OPTIONS)
+  .then(() => {
+    console.log("Mongodb connected--->");
+  })
+  .catch((err) => {
+    console.error("Connection failed: ", err);
+  });
 require('./employee.model');
